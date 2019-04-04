3517dcc50d0f584eb0b4ed575cfe8b4417831ec9
David Knoernschild <david@seattleweb.com>
2019-04-03 17:11:32 -0700

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,2 +1,12 @@
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Add-Content 'c:\test.txt' 'Hello from post install thingy'
+Add-Content 'c:\postinstall.txt' 'Hello from post install thingy'
+Add-Content 'c:\postinstall.txt' 'installing chocolatey'
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Add-Content 'c:\postinstall.txt' 'installing firefox'
+
+choco install firefox -y
+
+Add-Content 'c:\postinstall.txt' 'done'
