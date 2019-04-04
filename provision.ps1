81644f3a3001708c7373987e2a571bf19d5be102
David Knoernschild <david@seattleweb.com>
2019-04-03 23:20:07 -0700

    update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,12 +1,29 @@
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Add-Content 'c:\postinstall.txt' 'Hello from post install thingy'
-Add-Content 'c:\postinstall.txt' 'installing chocolatey'
+if (!(Test-Path '.\Desktop\postinstall.txt'))
+{
+  echo 'First pass, installing'
 
+  Add-Content 'Desktop\postinstall.txt' 'enabling openSSH'
+  Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  Add-Content 'Desktop\postinstall.txt' 'installing chocolatey'
+  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Add-Content 'c:\postinstall.txt' 'installing firefox'
+  Add-Content 'Desktop\postinstall.txt' 'installing firefox'
+  choco install firefox -y
 
-choco install firefox -y
+  Add-Content 'Desktop\postinstall.txt' 'installing gecko selenium driver'
+  choco install selenium-gecko-driver
 
-Add-Content 'c:\postinstall.txt' 'done'
+  Add-Content 'Desktop\postinstall.txt' 'installing nodejs'
+  choco install nodejs
+
+  Add-Content 'Desktop\postinstall.txt' 'restarting host'
+  Restart-Computer
+}
+else
+{
+  echo 'Second pass, starting OpenSSH'
+
+  Add-Content 'Desktop\postinstall.txt' 'starting sshd'
+  Start-Service sshd
+}
