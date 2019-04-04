a2a1c3d86b38245a9b5055f62f6ffc0b228cbef6
David Knoernschild <david@seattleweb.com>
2019-04-04 00:02:13 -0700

    update provision

--- provision.ps1
+++ provision.ps1
@@ -1,29 +1,20 @@
-if (!(Test-Path '.\Desktop\postinstall.txt'))
-{
-  echo 'First pass, installing'
+Add-Content '.\postinstall.txt' 'enabling openSSH'
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-  Add-Content 'Desktop\postinstall.txt' 'enabling openSSH'
-  Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Add-Content '.\postinstall.txt' 'creating startup ps1 file'
+Add-Content 'C:\Users\davidk\Desktop\startup.ps1' 'Start-Service sshd'
 
-  Add-Content 'Desktop\postinstall.txt' 'installing chocolatey'
-  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Add-Content '.\postinstall.txt' 'creating startup cmd file'
+Add-Content 'C:\Users\davidk\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd' 'PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> ".\StartupLog.txt" 2>&1 PowerShell C:\Users\davidk\Desktop\startup.ps1 >> ".\StartupLog.txt" 2>&1'
 
-  Add-Content 'Desktop\postinstall.txt' 'installing firefox'
-  choco install firefox -y
+Add-Content '.\postinstall.txt' 'installing chocolatey'
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-  Add-Content 'Desktop\postinstall.txt' 'installing gecko selenium driver'
-  choco install selenium-gecko-driver
+Add-Content '.\postinstall.txt' 'installing firefox'
+choco install firefox -y
 
-  Add-Content 'Desktop\postinstall.txt' 'installing nodejs'
-  choco install nodejs
+Add-Content '.\postinstall.txt' 'installing gecko selenium driver'
+choco install selenium-gecko-driver
 
-  Add-Content 'Desktop\postinstall.txt' 'restarting host'
-  Restart-Computer
-}
-else
-{
-  echo 'Second pass, starting OpenSSH'
-
-  Add-Content 'Desktop\postinstall.txt' 'starting sshd'
-  Start-Service sshd
-}
+Add-Content '.\postinstall.txt' 'installing nodejs'
+choco install nodejs
