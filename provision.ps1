c3197f716ee1d42d99e9a2c049928995afdcecb0
David Knoernschild <david@seattleweb.com>
2019-04-05 07:33:03 -0700

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -1,10 +1,3 @@
-Add-Content 'C:\postinstall.txt' 'enabling openSSH'
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-
-Add-Content 'C:\postinstall.txt' 'scheduling sshd startup on reboot'
-$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-Register-ScheduledJob -Trigger $trigger -ScriptBlock {Start-Service sshd} -Name StartOpenSSH >> 'C:\postinstall.txt' 2>&1
-
 Add-Content 'C:\postinstall.txt' 'installing chocolatey'
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) >> 'C:\postinstall.txt' 2>&1
 
@@ -16,5 +9,8 @@ choco install selenium-gecko-driver -y >> 'C:\postinstall.txt' 2>&1
 
 Add-Content 'C:\postinstall.txt' 'installing nodejs'
 choco install nodejs -y >> 'C:\postinstall.txt' 2>&1
+
+Add-Content 'C:\postinstall.txt' 'installing openssh'
+choco install openssh -params '"/SSHServerFeature /KeyBasedAuthenticationFeature"' -y >> 'C:\postinstall.txt' 2>&1
 
 Add-Content 'C:\postinstall.txt' 'postinstall complete'
