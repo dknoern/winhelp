4357fa61a937c0eaf6bb6cbdc3b7efc57097cd5b
David Knoernschild <david@seattleweb.com>
2019-04-09 10:15:53 -0700

    Update provision.ps1

--- provision.ps1
+++ provision.ps1
@@ -7,8 +7,11 @@ choco install firefox -y >> 'C:\postinstall.txt' 2>&1
 Add-Content 'C:\postinstall.txt' 'installing gecko selenium driver'
 choco install selenium-gecko-driver -y >> 'C:\postinstall.txt' 2>&1
 
+#Add-Content 'C:\postinstall.txt' 'installing git'
+choco install git -y >> 'C:\postinstall.txt' 2>&1
+
 #Add-Content 'C:\postinstall.txt' 'installing nodejs'
-#choco install nodejs -y >> 'C:\postinstall.txt' 2>&1
+choco install nodejs -y >> 'C:\postinstall.txt' 2>&1
 
 Add-Content 'C:\postinstall.txt' 'installing openssh'
 choco install openssh -params '"/SSHServerFeature /KeyBasedAuthenticationFeature"' -y >> 'C:\postinstall.txt' 2>&1
